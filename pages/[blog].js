32651f64481f074cf0866f7ca89477688a6d190c
Jaideep25 <jaideepch@outlook.com>
2021-08-01 11:28:29 +0530

    Initial sharing

---
+++ pages/[blog].js
@@ -0,0 +1,319 @@
+import Head from "next/head"
+import Image from "next/image"
+import axios from "axios"
+import { useState } from "react"
+import { Fade, Bounce, Slide } from "react-awesome-reveal"
+import slugify from "slugify"
+import Typist from "react-typist"
+import { CopyToClipboard } from "react-copy-to-clipboard"
+import { ToastContainer, toast } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+
+const Blog = ({ santhosh }) => {
+  const wishername = santhosh[0].content
+  const slugname = santhosh[0].slug
+  const pathname = "https://friendshipj25.up.railway.app/" + slugname + "/"
+  const sharetext = encodeURIComponent(
+    wishername + " Wishing you a Happy Friendship day " + pathname
+  )
+  const Whatsappshare = `https://api.whatsapp.com/send/?text=${sharetext}`
+  const Telegramshare =
+    "https://telegram.me/share/url?url=" +
+    pathname +
+    "&text=" +
+    wishername +
+    " Wishing you a Happy Friendship day"
+  const copied = () => {
+    toast.dark("📝 Copied", {
+      position: "top-right",
+      autoClose: 900,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      closeButton: false,
+    })
+  }
+  //toast.success("😊 \n" + wishername, {
+  //  position: "top-right",
+  //  autoClose: 2500,
+  //  hideProgressBar: true,
+  //  closeOnClick: true,
+  //  pauseOnHover: true,
+  //  draggable: true,
+  //  progress: undefined,
+  //  closeButton: false,
+  //})
+  const [username, setInput] = useState("")
+  const subscribe = e => {
+    e.preventDefault()
+    if (username == 0) {
+      console.log("Empty Title or Message")
+      toast.error("🤖 Empty Name", {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        closeButton: false,
+      })
+      return false
+    }
+    //const users = encodeURIComponent(input)
+    const users = slugify(username, {
+      replacement: "-",
+      remove: /[*+~.()'"!:@]/g,
+      lower: false,
+      strict: false,
+    })
+    window.location.href = `/${users}/`
+  }
+  return (
+    <section className="section">
+      <Head>
+        <meta name="HandheldFriendly" content="True" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/logo192.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <title>{`${
+          wishername + " Wishing you a Happy Friendship day 💛"
+        }`}</title>
+        <meta
+          name="description"
+          content="Happy Friendship day 💚 - Friendship Day Wishes and Greetings."
+        />
+        <meta
+          property="og:title"
+          content={`${wishername + " Wishing you a Happy Friendship day 💛"}`}
+        />
+        <meta
+          property="og:site_name"
+          content={`${wishername + " Wishing you a Happy Friendship day 💛"}`}
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:description"
+          content="Happy Friendship day 💚 - Friendship Day Wishes and Greetings."
+        />
+        <meta
+          property="og:image"
+          content="https://friendshipj25.up.railway.app/sanwebwish.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content={`${wishername + " Wishing you a Happy Friendship day 💛"}`}
+        />
+        <meta
+          name="twitter:description"
+          content="Happy Friendship day 💚 - Friendship Day Wishes and Greetings."
+        />
+        <meta
+          name="twitter:image"
+          content="https://friendshipj25.up.railway.app/sanwebwish.png"
+        />
+        <link rel="canonical" href={pathname} />
+        <meta name="twitter:url" content={pathname} />
+        <meta property="og:url" content={pathname} />
+
+        <link rel="preconnect" href="//fonts.googleapis.com" />
+        <link rel="preconnect" href="//cdnjs.cloudflare.com" />
+        <link rel="preconnect" href="//fonts.gstatic.com" />
+        <link rel="preconnect" href="https://www.googletagmanager.com" />
+        <link rel="preconnect" href="https://analytics.google.com" />
+
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "WebSite",
+              publisher: {
+                "@type": "Organization",
+                name: `${wishername} Wishing you a Happy Friendship day 💛`,
+                url: `${pathname}`,
+                logo: {
+                  "@type": "ImageObject",
+                  url: "https://friendshipj25.up.railway.app/logo.png",
+                  width: 60,
+                  height: 60,
+                },
+              },
+              url: `${pathname}`,
+              image: {
+                "@type": "ImageObject",
+                url: "https://friendshipj25.up.railway.app/sanwebwish.png",
+                width: 1200,
+                height: 630,
+              },
+              mainEntityOfPage: {
+                "@type": "WebPage",
+                "@id": `${pathname}`,
+              },
+              description:
+                "Happy Friendship day 💚 - Friendship Day Wishes and Greetings.",
+            }),
+          }}
+        />
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-MG7YXQ53CG"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            if(true) {
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-MG7YXQ53CG', {
+                'page_title' : '${wishername} Wishing you a Happy Friendship day 💛',
+                'page_location': '${pathname}'
+              });
+             }`,
+          }}
+        />
+      </Head>
+      <div className="container content">
+        <div className="columns is-centered">
+          <div className="column is-half">
+            <Slide triggerOnce>
+              <br />
+              <h1 className="title is-6 has-text-warning has-text-centered">
+                <Typist
+                  avgTypingDelay={40}
+                  startDelay={1800}
+                  cursor={{ hideWhenDone: true }}
+                >
+                  Happy Friendship day 💛
+                </Typist>
+              </h1>
+              <br />
+              <Bounce>
+                <p className="has-text-centered">💚 {wishername} 💚</p>
+              </Bounce>
+            </Slide>
+            <br />
+            <Slide triggerOnce>
+              <Fade>
+                <figure className="image is-centered">
+                  <Image
+                    src="/friends.png"
+                    alt="Happy Friendship Day"
+                    width={128}
+                    height={128}
+                  />
+                </figure>
+              </Fade>
+            </Slide>
+            <br />
+            <Slide triggerOnce>
+              <div className="notification has-text-weight-bold read-more">
+                <p className="has-text-weight-bold has-text-left">
+                  We have shared laughter 💚
+                  <br />
+                  <br />
+                  We shared tears
+                  <br />
+                  <br />
+                  We shared disappointment over the years
+                  <br />
+                  <br />
+                  So on this friendship day my wish for you
+                  <br />
+                  <br />
+                  Is success and happiness in all you do.
+                  <br />
+                  <br />
+                  Happy Friendship day 💗
+                </p>
+              </div>
+            </Slide>
+            <br />
+            <div className="buttons is-centered">
+              <ToastContainer />
+              <CopyToClipboard text={pathname}>
+                <button
+                  className="button is-danger read-random"
+                  onClick={copied}
+                >
+                  📝 Copy URL
+                </button>
+              </CopyToClipboard>
+            </div>
+            <hr />
+            <div className="buttons is-centered">
+              <a
+                className="button is-success read-random"
+                href={Whatsappshare}
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+              >
+                🍔 Whatsapp
+              </a>
+              <a
+                className="button is-info read-random"
+                href={Telegramshare}
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+              >
+                🤖 Telegram
+              </a>
+            </div>
+            <hr />
+            <br />
+            <div className="notification is-warning has-text-weight-bold">
+              <br />
+              <h3 className="has-text-centered">Create your Wish</h3>
+              <div className="has-text-centered">
+                <small>Enter Your Name</small>
+              </div>
+              <br />
+              <div className="control">
+                <input
+                  className="input"
+                  id="username"
+                  name="username"
+                  type="text"
+                  placeholder="Enter Name"
+                  value={username}
+                  onChange={e => setInput(e.target.value)}
+                  required
+                />
+              </div>
+              <br />
+              <button
+                onClick={subscribe}
+                className="button is-link read-random sign-button"
+                type="submit"
+              >
+                Create Wishes
+              </button>
+            </div>
+            <ToastContainer />
+            <br />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Blog
+
+Blog.getInitialProps = async context => {
+  const { blog } = context.query
+  const res = await axios(
+    "https://friendshipj25.up.railway.app/api/wish/" + encodeURIComponent(blog)
+  )
+  const data = await res.data
+
+  return { santhosh: data }
+}
---
+++ pages/index.js
@@ -0,0 +1,248 @@
+import Head from "next/head"
+import Image from "next/image"
+import React, { Component } from "react"
+import { Fade, Slide } from "react-awesome-reveal"
+import Typist from "react-typist"
+import slugify from "slugify"
+import { ToastContainer, toast } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+
+class RetrieveList extends Component {
+  componentDidMount() {
+    if (document.getElementById("btnSignUp") != null) {
+      document.getElementById("btnSignUp").addEventListener("click", e => {
+        const name = document.getElementById("name").value
+        /* eslint eqeqeq: 0 */
+        if (name == 0) {
+          console.log("Empty Title or Message")
+          toast.error("🤖 Empty Name", {
+            position: "top-right",
+            autoClose: 1000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            closeButton: false,
+          })
+          return false
+        }
+        //const users = encodeURIComponent(name)
+        const users = slugify(name, {
+          replacement: "-",
+          remove: /[*+~.()'"!:@]/g,
+          lower: false,
+          strict: false,
+        })
+        window.location.href = `/${users}/`
+      })
+    }
+  }
+  PageToast() {
+    toast.dark("📝 Copied", {
+      position: "top-right",
+      autoClose: 900,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      closeButton: false,
+    })
+  }
+
+  render() {
+    const pathname = "https://friendshipj25.up.railway.app/"
+    return (
+      <section className="section">
+        <Head>
+          <meta name="HandheldFriendly" content="True" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" href="/logo192.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="theme-color" content="#000000" />
+
+          <title>Happy Friendship day 💛 Wishes</title>
+          <meta
+            name="description"
+            content="Happy Friendship day 💚 - Friendship Day Wishes and Greetings."
+          />
+          <meta property="og:title" content="Happy Friendship day 💛 Wishes" />
+          <meta
+            property="og:site_name"
+            content="Happy Friendship day 💛 Wishes"
+          />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:description"
+            content="Happy Friendship day 💚 - Friendship Day Wishes and Greetings."
+          />
+          <meta
+            property="og:image"
+            content="https://friendshipj25.up.railway.app/sanwebwish.png"
+          />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="Happy Friendship day 💛 Wishes" />
+          <meta
+            name="twitter:description"
+            content="Happy Friendship day 💚 - Friendship Day Wishes and Greetings."
+          />
+          <meta
+            name="twitter:image"
+            content="https://friendshipj25.up.railway.app/sanwebwish.png"
+          />
+          <link rel="canonical" href={pathname} />
+          <meta name="twitter:url" content={pathname} />
+          <meta property="og:url" content={pathname} />
+
+          <link rel="preconnect" href="//fonts.googleapis.com" />
+          <link rel="preconnect" href="//cdnjs.cloudflare.com" />
+          <link rel="preconnect" href="//fonts.gstatic.com" />
+          <link rel="preconnect" href="https://www.googletagmanager.com" />
+          <link rel="preconnect" href="https://analytics.google.com" />
+
+          <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{
+              __html: JSON.stringify({
+                "@context": "https://schema.org",
+                "@type": "WebSite",
+                publisher: {
+                  "@type": "Organization",
+                  name: "Happy Friendship day 💛 Wishes",
+                  url: `${pathname}`,
+                  logo: {
+                    "@type": "ImageObject",
+                    url: "https://friendshipj25.up.railway.app/logo.png",
+                    width: 60,
+                    height: 60,
+                  },
+                },
+                url: `${pathname}`,
+                image: {
+                  "@type": "ImageObject",
+                  url: "https://friendshipj25.up.railway.app/sanwebwish.png",
+                  width: 1200,
+                  height: 630,
+                },
+                mainEntityOfPage: {
+                  "@type": "WebPage",
+                  "@id": `${pathname}`,
+                },
+                description:
+                  "Happy Friendship day 💚 - Friendship Day Wishes and Greetings.",
+              }),
+            }}
+          />
+
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-MG7YXQ53CG"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            if(true) {
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-MG7YXQ53CG', {
+                'page_title' : 'Happy Friendship day 💛 Wishes - Home',
+                'page_location': '${pathname}'
+              });
+             }`,
+            }}
+          />
+        </Head>
+        <div className="container content">
+          <div className="columns is-centered">
+            <div className="column is-half">
+              <Slide triggerOnce>
+                <br />
+                <h1 className="title is-6 has-text-warning has-text-centered">
+                  <Typist
+                    avgTypingDelay={40}
+                    startDelay={1800}
+                    cursor={{ hideWhenDone: true }}
+                  >
+                    Happy Friendship day 💛
+                  </Typist>
+                </h1>
+              </Slide>
+              <br />
+              <Slide triggerOnce>
+                <Fade>
+                  <figure className="image is-centered">
+                    <Image
+                      src="/friends.png"
+                      alt="Happy Friendship Day"
+                      width={128}
+                      height={128}
+                    />
+                  </figure>
+                </Fade>
+              </Slide>
+              <br />
+              <Slide triggerOnce>
+                <div className="notification has-text-weight-bold read-more">
+                  <p className="has-text-weight-bold has-text-left">
+                    Hi Thanks for using our Web App 💚
+                    <br />
+                    <br />
+                    Send Happy Friendship Day Wishes to your Friends
+                    <br />
+                    <br />
+                    Free Text Wish Greetings
+                    <br />
+                    <br />
+                    ZERO ADS - No Tricks and Ad Clicks
+                    <br />
+                    <br />
+                    Just Enter the Name and Create a Wish
+                    <br />
+                    <br />
+                    Happy Friendship day 💗
+                  </p>
+                </div>
+              </Slide>
+              <br />
+              <br />
+              <div className="notification is-warning has-text-weight-bold">
+                <br />
+                <h3 className="has-text-centered">Create your Wish</h3>
+                <div className="has-text-centered">
+                  <small>Enter Your Name</small>
+                </div>
+                <br />
+                <div className="control">
+                  <input
+                    className="input"
+                    id="name"
+                    name="name"
+                    type="text"
+                    placeholder="Enter Name"
+                    required
+                  />
+                </div>
+                <br />
+                <button
+                  id="btnSignUp"
+                  className="button is-link read-random sign-button"
+                  type="submit"
+                >
+                  Create Wishes
+                </button>
+                <ToastContainer />
+                <br />
+              </div>
+              <br />
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
+export default RetrieveList
